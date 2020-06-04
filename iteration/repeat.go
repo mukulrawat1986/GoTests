fa84316a1c0f21d7a2b764fe19c7c2ea3b1e0029
Mukul Rawat <mukulsrawat@gmail.com>
2020-06-04 22:38:16 +0530

    add repeatCount argument
    change the function and test so a caller can specify how many times a character is to be repeated

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,9 @@
 package main
 
 // Repeat takes a character and repeats it 5 times
-func Repeat(character string) string {
+func Repeat(character string, repeatedCount int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < repeatedCount; i++ {
 		repeated += character
 	}
 	return repeated
